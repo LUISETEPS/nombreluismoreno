2ec9a3c0ad19d5caa1a94cb0e4b2dbf1d89d626a
LUISETEPS <132657026+LUISETEPS@users.noreply.github.com>
2023-05-10 19:17:45 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
-        # . . . .
-        # . . . .
-        # . . . .
-        # . . . .
-        # # # # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # . . . #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # # # # #
-        . . # . .
-        . . # . .
-        . . # . .
-        # # # # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # # # # #
-        . . . . #
-        # # # # #
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
         # . . . #
         # # . # #
         # . # . #
@@ -80,9 +47,56 @@ input.onButtonPressed(Button.B, function () {
         # # # # #
         `)
 })
-if (input.buttonIsPressed(Button.A)) {
-	
-}
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("LUIS")
+    basic.pause(100)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(800)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(800)
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # # # # .
+        # . . # .
+        # . . . #
+        `)
+    basic.pause(800)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(800)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.pause(800)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
 })
